acb83b83b7b96d1389f75e3102c087a98731399b
JOSU� ACOSTA LONDO�O <josueacosta261853@correo.itm.edu.co>
2023-02-15 18:45:58 -0500

    aad to Employee her ToString

---
+++ MyFirstCodeOOP/MyFirstCodeOOP/Employee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstCodeOOP
+{
+    public abstract class Employee
+    {
+        #region Properties
+        public int Id { get; set; }
+        public  string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public DateTime HiringDate { get; set; }
+        public bool IsActive { get; set; }
+
+        #region Methods
+        public abstract decimal GetValueToPay();
+        public override string ToString()
+        {
+            return $"---EMPLOYEE---\n\t " +
+                $"ID: {Id} \n\t" +
+                $" First Name:  {FirstName} \n\t " +
+                $"Last Name: {LastName} \n\t " +
+                $"Birth Date: {BirthDate} \n\t " +
+                $"Hiring Date: {HiringDate} \n\t " +
+                $"Is Active?: {IsActive}"; 
+        }
+        #endregion
+        #endregion
+    }
+}
