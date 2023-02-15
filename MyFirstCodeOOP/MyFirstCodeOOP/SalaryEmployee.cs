4de46c168ee88441e0e474137dd28e6dae9fe8ad
JOSU� ACOSTA LONDO�O <josueacosta261853@correo.itm.edu.co>
2023-02-15 18:56:50 -0500

    Creating new correct class called SalaryEmployee

---
+++ MyFirstCodeOOP/MyFirstCodeOOP/SalaryEmployee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstCodeOOP
+{
+    public class SalaryEmployee : Employee
+    {
+        #region Properties
+        public decimal Salary { get; set; }
+        #endregion
+
+        #region Methods
+        public override decimal GetValueToPay()
+        {
+            return  Salary;
+        }
+        public override string ToString() {
+            return $"{base.ToString} \n\t " +
+                $"Value to pay: {GetValueToPay()}";
+        }
+        #endregion
+    }
+}
